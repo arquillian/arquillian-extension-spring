33ab152e613522d3dd2d72523eea93c24a5e9c8b
Jakub Narloch <jmnarloch@gmail.com>
2012-04-17 20:46:15 +0200

    Added unit tests.

---
+++ src/test/java/org/jboss/arquillian/spring/client/SpringEnricherExtensionTestCase.java
@@ -0,0 +1,80 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.spring.client;
+
+import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
+import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
+import org.jboss.arquillian.core.spi.LoadableExtension;
+import org.jboss.arquillian.spring.SpringExtensionConsts;
+import org.jboss.arquillian.spring.annotations.SpringConfiguration;
+import org.jboss.arquillian.spring.annotations.SpringWebConfiguration;
+import org.jboss.arquillian.spring.container.ApplicationContextProducer;
+import org.jboss.arquillian.spring.container.SpringEnricherRemoteExtension;
+import org.jboss.arquillian.spring.container.SpringInjectionEnricher;
+import org.jboss.arquillian.test.spi.TestEnricher;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * <p>Tests {@link SpringEnricherExtension} class.</p>
+ *
+ * @author <a href="mailto:jmnarloch@gmail.com">Jakub Narloch</a>
+ */
+public class SpringEnricherExtensionTestCase {
+
+    /**
+     * <p>Represents the instance of tested class.</p>
+     */
+    private SpringEnricherExtension instance;
+
+    /**
+     * <p>Sets up the test environment.</p>
+     */
+    @Before
+    public void setUp() {
+
+        instance = new SpringEnricherExtension();
+    }
+
+    /**
+     * <p>Tests the {@link SpringEnricherExtension#register(LoadableExtension.ExtensionBuilder)} method.</p>
+     */
+    @Test
+    public void testRegister() {
+
+        LoadableExtension.ExtensionBuilder mockExtensionBuilder = mock(LoadableExtension.ExtensionBuilder.class);
+        when(mockExtensionBuilder.service(any(Class.class), any(Class.class))).thenReturn(mockExtensionBuilder);
+
+        instance.register(mockExtensionBuilder);
+
+        verify(mockExtensionBuilder).service(AuxiliaryArchiveAppender.class, SpringEnricherArchiveAppender.class);
+        verify(mockExtensionBuilder).service(ProtocolArchiveProcessor.class, SpringProtocolArchiveProcessor.class);
+        verify(mockExtensionBuilder).service(TestEnricher.class, SpringInjectionEnricher.class);
+    }
+}
---
+++ src/test/java/org/jboss/arquillian/spring/container/SpringEnricherRemoteExtensionTest.java
@@ -0,0 +1,82 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.spring.container;
+
+import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
+import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
+import org.jboss.arquillian.core.spi.LoadableExtension;
+import org.jboss.arquillian.spring.SpringExtensionConsts;
+import org.jboss.arquillian.spring.annotations.SpringConfiguration;
+import org.jboss.arquillian.spring.annotations.SpringWebConfiguration;
+import org.jboss.arquillian.spring.client.SpringEnricherArchiveAppender;
+import org.jboss.arquillian.spring.client.SpringEnricherExtension;
+import org.jboss.arquillian.spring.client.SpringProtocolArchiveProcessor;
+import org.jboss.arquillian.test.spi.TestEnricher;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePath;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * <p>Tests {@link SpringEnricherRemoteExtension} class.</p>
+ *
+ * @author <a href="mailto:jmnarloch@gmail.com">Jakub Narloch</a>
+ */
+public class SpringEnricherRemoteExtensionTest {
+
+    /**
+     * <p>Represents the instance of tested class.</p>
+     */
+    private SpringEnricherRemoteExtension instance;
+
+    /**
+     * <p>Sets up the test environment.</p>
+     */
+    @Before
+    public void setUp() {
+
+        instance = new SpringEnricherRemoteExtension();
+    }
+
+    /**
+     * <p>Tests the {@link SpringEnricherRemoteExtension#register(LoadableExtension.ExtensionBuilder)} method.</p>
+     */
+    @Test
+    public void testRegister() {
+
+        LoadableExtension.ExtensionBuilder mockExtensionBuilder = mock(LoadableExtension.ExtensionBuilder.class);
+        when(mockExtensionBuilder.service(any(Class.class), any(Class.class))).thenReturn(mockExtensionBuilder);
+        when(mockExtensionBuilder.observer(any(Class.class))).thenReturn(mockExtensionBuilder);
+
+        instance.register(mockExtensionBuilder);
+
+        verify(mockExtensionBuilder).service(TestEnricher.class, SpringInjectionEnricher.class);
+        verify(mockExtensionBuilder).observer(ApplicationContextProducer.class);
+    }
+}
