cee73c9d52773268195d5ac3ac6156af4f348cc5
Jakub Narloch <jmnarloch@gmail.com>
2012-04-22 00:55:34 +0200

    Changed the project structure: introduced common, spring-2.5, spring-3 modules.

---
+++ arquillian-spring-2.5/src/test/java/org/jboss/arquillian/spring/container/SpringEnricherRemoteExtensionTestCase.java
@@ -0,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.spring.container;
+
+import org.jboss.arquillian.core.spi.LoadableExtension;
+import org.jboss.arquillian.spring.context.ApplicationContextDestroyer;
+import org.jboss.arquillian.spring.context.WebApplicationContextProducer;
+import org.jboss.arquillian.spring.context.XmlApplicationContextProducer;
+import org.jboss.arquillian.spring.testenricher.SpringInjectionEnricher;
+import org.jboss.arquillian.test.spi.TestEnricher;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * <p>Tests {@link org.jboss.arquillian.spring.container.SpringEnricherRemoteExtension} class.</p>
+ *
+ * @author <a href="mailto:jmnarloch@gmail.com">Jakub Narloch</a>
+ */
+public class SpringEnricherRemoteExtensionTestCase {
+
+    /**
+     * <p>Represents the instance of tested class.</p>
+     */
+    private SpringEnricherRemoteExtension instance;
+
+    /**
+     * <p>Sets up the test environment.</p>
+     */
+    @Before
+    public void setUp() {
+
+        instance = new SpringEnricherRemoteExtension();
+    }
+
+    /**
+     * <p>Tests the {@link org.jboss.arquillian.spring.container.SpringEnricherRemoteExtension#register(LoadableExtension.ExtensionBuilder)}
+     * method.</p>
+     */
+    @Test
+    public void testRegister() {
+
+        LoadableExtension.ExtensionBuilder mockExtensionBuilder = mock(LoadableExtension.ExtensionBuilder.class);
+        when(mockExtensionBuilder.service(any(Class.class), any(Class.class))).thenReturn(mockExtensionBuilder);
+        when(mockExtensionBuilder.observer(any(Class.class))).thenReturn(mockExtensionBuilder);
+
+        instance.register(mockExtensionBuilder);
+
+        verify(mockExtensionBuilder).service(TestEnricher.class, SpringInjectionEnricher.class);
+        verify(mockExtensionBuilder).observer(XmlApplicationContextProducer.class);
+        verify(mockExtensionBuilder).observer(WebApplicationContextProducer.class);
+        verify(mockExtensionBuilder).observer(ApplicationContextDestroyer.class);
+    }
+}
---
+++ arquillian-spring-3/src/main/java/org/jboss/arquillian/spring/context/AnnotatedApplicationContextProducer.java
@@ -0,0 +1,97 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.spring.context;
+
+import org.jboss.arquillian.spring.annotations.SpringAnnotatedConfiguration;
+import org.jboss.arquillian.test.spi.TestClass;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * <p>The {@link AbstractApplicationContextProducer} that creates the {@link org.springframework.context.annotation.AnnotationConfigApplicationContext}
+ * instance.</p>
+ *
+ * @author <a href="mailto:jmnarloch@gmail.com">Jakub Narloch</a>
+ * @version $Revision: $
+ */
+public class AnnotatedApplicationContextProducer extends AbstractApplicationContextProducer {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean supports(TestClass testClass) {
+        return testClass.isAnnotationPresent(SpringAnnotatedConfiguration.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected TestScopeApplicationContext createApplicationContext(TestClass testClass) {
+
+        return new TestScopeApplicationContext(getApplicationContext(testClass), true);
+    }
+
+    /**
+     * <p>Creates the application context.</p>
+     *
+     * @param testClass the test class
+     *
+     * @return created {@link org.springframework.context.ApplicationContext}
+     */
+    private ApplicationContext getApplicationContext(TestClass testClass) {
+
+        SpringAnnotatedConfiguration springConfiguration = testClass.getAnnotation(SpringAnnotatedConfiguration.class);
+
+        String[] packages = springConfiguration.packages();
+        Class<?>[] classes = springConfiguration.classes();
+        if (packages.length > 0 || classes.length > 0) {
+
+            return createAnnotatedApplicationContext(classes, packages);
+        }
+
+        throw new RuntimeException("The test: " + testClass.getName()
+                + " annotated with SpringAnnotatedConfiguration must specify the configuration classes or packages.");
+    }
+
+    /**
+     * <p>Creates instance of {@link org.springframework.context.annotation.AnnotationConfigApplicationContext} class.</p>
+     *
+     * @param classes  the annotated classes to register
+     * @param packages the packages containing the annotated classes
+     *
+     * @return the created instance of {@link org.springframework.context.annotation.AnnotationConfigApplicationContext}
+     */
+    private ApplicationContext createAnnotatedApplicationContext(Class<?>[] classes, String[] packages) {
+
+        if (classes.length > 0) {
+            AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(classes);
+
+            if (packages.length > 0) {
+                applicationContext.scan(packages);
+                applicationContext.refresh();
+            }
+
+            return applicationContext;
+
+        } else {
+
+            return new AnnotationConfigApplicationContext(packages);
+        }
+    }
+}
