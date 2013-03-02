e8f3fe35e92564c54dca2774c8a4e2c54812224a
Jakub Kurlenda <j.kurlenda@partner.samsung.com>
2013-03-02 21:12:00 +0100

    [ARQ-1305] Defaults values for @SpringConfiguration and @SpringAnnotationConfiguration what: added test @RunWith(Arquillian.class) why: to verify that default configuration gets injected into test classes

---
+++ arquillian-service-integration-spring-3-int-tests/src/test/java/org/jboss/arquillian/spring/testsuite/test/AnnotatedConfigurationDefaultConfigTestCase.java
@@ -0,0 +1,64 @@
+package org.jboss.arquillian.spring.testsuite.test;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.spring.integration.test.annotation.SpringClientAnnotationConfiguration;
+import org.jboss.arquillian.spring.testsuite.beans.model.Employee;
+import org.jboss.arquillian.spring.testsuite.beans.repository.EmployeeRepository;
+import org.jboss.arquillian.spring.testsuite.beans.repository.impl.DefaultEmployeeRepository;
+import org.jboss.arquillian.spring.testsuite.beans.service.EmployeeService;
+import org.jboss.arquillian.spring.testsuite.beans.service.impl.DefaultEmployeeService;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import static org.jboss.arquillian.spring.testsuite.test.Deployments.createAppDeployment;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+@SpringClientAnnotationConfiguration
+public class AnnotatedConfigurationDefaultConfigTestCase {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    /**
+     * <p>Creates the test deployment.</p>
+     *
+     * @return the test deployment
+     */
+    @Deployment
+    public static Archive createTestArchive() {
+        return createAppDeployment().addClass(DefaultConfiguration.class);
+    }
+
+    @Test
+    public void testGetEmployees() {
+        List<Employee> result = employeeService.getEmployees();
+
+        assertNotNull("Method returned null list as result.", result);
+        assertEquals("Two employees were expected.", 2, result.size());
+    }
+
+    @Configuration
+    public static class DefaultConfiguration {
+
+        @Bean
+        public EmployeeRepository defaultEmployeeRepository() {
+            System.out.println("defaultEmployeeRepositorydefaultEmployeeRepository");
+            return new DefaultEmployeeRepository();
+        }
+
+        @Bean
+        public EmployeeService defaultEmployeeService() {
+            System.out.println("defaultEmployeeServicedefaultEmployeeServicedefaultEmployeeService");
+            return new DefaultEmployeeService();
+        }
+    }
+}
